26e6a6dd41242ecdd9029e34ad71b84f0ff5f0e3
Shubham Garg <shbhmgarg@gmail.com>
2021-12-03 00:15:17 +0530

    Navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,23 @@
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Navbar from "./components/layout/Navbar";
+import Home from "./components/pages/Home";
+import About from "./components/pages/About";
 
 const App = () => {
   return (
-    <div className="App">
-      My app    
-    </div>
+    <Router>
+      <>
+        <Navbar />
+        <div className="container">
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <Route exact path='/about' component={About} />
+          </Switch>
+        </div>
+      </>
+    </Router>
   );
-}
+};
 
 export default App;
