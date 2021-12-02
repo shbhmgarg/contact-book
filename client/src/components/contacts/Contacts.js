508041de754c57ddfc812fa84606fe0f1c13cf5a
Shubham Garg <shbhmgarg@gmail.com>
2021-12-03 05:14:40 +0530

    contacts CRUD front end

---
+++ client/src/components/contacts/Contacts.js
@@ -0,0 +1,38 @@
+import React, { Fragment, useContext } from "react";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
+import ContactItem from "./ContactItem";
+import ContactContext from "../../context/contact/contactContext";
+
+const Contacts = () => {
+  const contactContext = useContext(ContactContext);
+
+  const { contacts, filtered } = contactContext;
+
+  if (contacts.length === 0) {
+    return <h4>Please add contacts</h4>;
+  }
+
+  return (
+    <Fragment>
+      <TransitionGroup>
+        {filtered !== null
+          ? filtered.map((contact) => {
+              return (
+                <CSSTransition key={contact.id} timeout={500} classNames='item'>
+                  <ContactItem contact={contact} />
+                </CSSTransition>
+              );
+            })
+          : contacts.map((contact) => {
+              return (
+                <CSSTransition key={contact.id} timeout={500} classNames='item'>
+                  <ContactItem contact={contact} />
+                </CSSTransition>
+              );
+            })}
+      </TransitionGroup>
+    </Fragment>
+  );
+};
+
+export default Contacts;
