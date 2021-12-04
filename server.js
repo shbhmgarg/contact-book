8263743a217704f9e0d54987989243eddbe5061a
Shubham Garg <shbhmgarg@gmail.com>
2021-12-05 04:39:35 +0530

    production config

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
-const connectDB = require('./config/db')
+const connectDB = require("./config/db");
+const path = require("path");
 
 const app = express();
 
@@ -12,9 +13,19 @@ app.use(express.json({ extended: false }));
 app.get("/", (req, res) => res.json({ msg: "Contact Keeper API" }));
 
 //Define routes
-app.use('/api/users', require('./routes/users'));
-app.use('/api/contacts', require('./routes/contacts'));
-app.use('/api/auth', require('./routes/auth'));
+app.use("/api/users", require("./routes/users"));
+app.use("/api/contacts", require("./routes/contacts"));
+app.use("/api/auth", require("./routes/auth"));
+
+// Serve static assets in productions
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("/*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  );
+}
 
 const port = process.env.PORT || 5000;
 
